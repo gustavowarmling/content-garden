6b8744d85dc47a1e5512e6fa0a8aa3bf67dbc8ec
Gustavo Warmling <gustavowarmling@gmail.com>
2021-12-22 22:12:55 -0300

    feat(home): get data from prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { getPrismicClient } from '../services/prismic'
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
 import { CardGroup } from '../components/CardGroup'
 import { Footer } from '../components/Footer'
@@ -9,14 +12,32 @@ import { PageWrapper, Title } from '../styles/general'
 
 import growthSVG from '../public/growth.svg'
 
-const Home: NextPage = () => {
-  const cardsData = [
-    { title: 'UX Design', emoji: `✏️`, slug: 'ux-design0' },
-    { title: 'UX Design', emoji: `✏️`, slug: 'ux-design1' },
-    { title: 'UX Design', emoji: `✏️`, slug: 'ux-design2' },
-    { title: 'UX Design', emoji: `✏️`, slug: 'ux-design3' },
-    { title: 'UX Design', emoji: `✏️`, slug: 'ux-design4' },
-  ]
+type Data = {
+  title: string
+  svg: string
+}
+
+type Category = {
+  slug: string
+  title: string
+  emoji: string
+}
+
+interface HomeProps {
+  homeProps: Data[]
+  categories: Category[]
+}
+
+export default function Home({ homeProps, categories = [] }: HomeProps) {
+  // const cardsData = [
+  //   { title: 'UX Design', emoji: `✏️`, slug: 'ux-design0' },
+  //   { title: 'UX Design', emoji: `✏️`, slug: 'ux-design1' },
+  //   { title: 'UX Design', emoji: `✏️`, slug: 'ux-design2' },
+  //   { title: 'UX Design', emoji: `✏️`, slug: 'ux-design3' },
+  //   { title: 'UX Design', emoji: `✏️`, slug: 'ux-design4' },
+  // ]
+
+  const { svg, title } = homeProps[0]
 
   return (
     <PageWrapper>
@@ -29,22 +50,64 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Title>
-        The biggest digital garden of Product Design in all internet.
-      </Title>
+      <Title>{title}</Title>
 
       <Image
         src={growthSVG}
-        alt="Desenho de um garoto regando a planta"
+        alt="Desenho de um garoto regando uma planta"
         height="1200px"
         width="1200px"
       />
 
-      <CardGroup cards={cardsData} />
+      <CardGroup cards={categories} />
 
       <Footer />
     </PageWrapper>
   )
 }
 
-export default Home
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const homeResponse = await prismic?.query(
+    [Prismic.predicates.at('document.type', 'home')],
+    {
+      fetch: ['home.title', 'home.svg'],
+      pageSize: 1,
+    }
+  )
+
+  const categoriesResponse = await prismic?.query(
+    [Prismic.predicates.at('document.type', 'category')],
+    {
+      fetch: ['category.uid', 'category.title', 'category.emoji'],
+      pageSize: 10,
+    }
+  )
+
+  const homeProps = homeResponse?.results.map((home) => {
+    const { title, svg } = home.data
+
+    return {
+      title: RichText.asText(title),
+      svg: svg.url,
+    }
+  })
+
+  const categories = categoriesResponse?.results.map((category) => {
+    const { title, emoji } = category.data
+
+    return {
+      slug: category.uid,
+      title: RichText.asText(title),
+      emoji: RichText.asText(emoji),
+    }
+  })
+
+  return {
+    props: {
+      homeProps,
+      categories,
+    },
+  }
+}
